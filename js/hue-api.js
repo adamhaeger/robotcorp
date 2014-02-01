e6896b32f8514bd2e7b29b3893fabb8d01573ed6
wuhado <wuh@netownage.net>
2014-02-01 14:05:03 +0100

    Some api changes

--- js/hue-api.js
+++ js/hue-api.js
@@ -3,9 +3,44 @@ var username = "newdeveloper";
 
 function apiFunction() {
 
+    this.convert = {
+        hue: function(accVal){
+            if (accVal > 1) {
+                accVal = 1;
+            } else if (accVal < -1) {
+                accVal = -1;
+            }
+
+            return parseInt(32767 * (accVal+1),10);
+        },
+        sat: function(accVal) {
+            if (accVal > 1) {
+                accVal = 1;
+            } else if (accVal < -1) {
+                accVal = -1;
+            }
+
+            return parseInt(127 * (accVal+1),10);
+        },
+        brightness: function(accVal) {
+            if (accVal > 1) {
+                accVal = 1;
+            } else if (accVal < -1) {
+                accVal = -1;
+            }
+
+            return parseInt(127 * (accVal+1),10);
+        }
+
+
+    }
 
     this.light = {
         on: function(lightId, type, hue, sat, brightness) {
+
+            if (lightId == 0) {
+                return api.group.on(lightId, type, hue, sat, brightness);
+            }
 
             if (type == undefined) {
                 type = "none";
@@ -41,6 +76,35 @@ function apiFunction() {
             });
         }
     };
+
+    this.group = {
+        on: function(groupId, type, hue, sat, brightness) {
+
+            if (type == undefined) {
+                type = "none";
+            }
+
+            var lightObj = {
+                "on":true,
+                "sat":sat,
+                "bri":brightness,
+                "hue":hue,
+                "alert":type,
+                "effect": "none"
+            };
+
+            return $.ajax({
+            // /api/newdeveloper/groups/0/action
+                url: baseUrl + "/api/" + username + "/groups/" + groupId+ "/action",
+                type: "PUT",
+                contentType: "application/jsonp",
+                data: JSON.stringify(lightObj)
+            });
+        },
+        off: function(groupId) {
+
+        }
+    }
 
 }
 
